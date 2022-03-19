13ed8d25bfe75ce47fa6fe7e5426efa00b3b1fa0
Timothy Moon <timhgmoon@gmail.com>
2022-03-19 01:21:28 -0700

    fixed image not filling div(small issue with left padding)

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Project1 from '../images/project1.jpg'
+import * as projectStyles from './projects.module.css'
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
@@ -7,13 +8,14 @@ import Row from 'react-bootstrap/Row';
 const Projects = () => {
   return(
     <Container>
-      <Row className="h-50">
+      My Projects
+      <Row>
+        <Col xs={6}>
+          <img src={Project1} alt="project 1" width="100%" height="250px"></img>
+        </Col>
         <Col xs={6}>
           <h1>Project Name</h1>
           <p>Project description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus ex eget nisl vestibulum dictum. Quisque felis libero, interdum pellentesque nunc malesuada, pellentesque elementum lacus. Nullam hendrerit magna quam, ultrices maximus augue commodo in. Proin efficitur augue orci. Maecenas eget diam mauris. Sed sagittis ultrices enim quis fringilla.</p>
-        </Col>
-        <Col xs={6}>
-          <img src={Project1} alt="project 1" fluid></img>
         </Col>
       </Row>
     </Container>
