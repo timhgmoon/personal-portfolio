849b1ff8581d2187ae3fcc5756415e40969a73b2
Timothy Moon <timhgmoon@gmail.com>
2022-03-27 21:42:40 -0700

    added social media links and icons from fontawesome

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import * as contactStyles from './contact.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
@@ -8,7 +10,8 @@ import Row from 'react-bootstrap/Row';
 
 const Contact = () => {
   const containerClass = `px-5 py-4 ${contactStyles.background}`
-  const formContainer = ` my-4 ${contactStyles.secondary}`
+  const formContainer = `p-2 my-4 ${contactStyles.secondary}`
+  const formContent = `d-flex flex-column justify-content-center ${contactStyles.contactInfo}`
   return(
     <Container className={containerClass} fluid>
       <h2 className={contactStyles.contactHeader}><span>Contact</span></h2>
@@ -28,13 +31,21 @@ const Contact = () => {
             </Button>
           </Form>
         </Col>
-        <Col sm={4} className="d-flex flex-column justify-content-center">
+        <Col sm={4} className={formContent}>
           <p>Tim Moon</p>
           <p>timhgmoon@gmail.com</p>
-          <p>Social Media</p>
+          <p>Social Media:</p>
           <ul>
-            <li>github(add links here)</li>
-            <li>linkedIn(add links here)</li>
+            <li>
+              <a href="https://github.com/timhgmoon" target="_blank">
+                <FontAwesomeIcon icon={faGithub} size="xl"/>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/timhgmoon/" target="_blank">
+                <FontAwesomeIcon icon={faLinkedin} size="xl" color="#2867B2"/>
+              </a>
+            </li>
           </ul>
         </Col>
       </Row>
