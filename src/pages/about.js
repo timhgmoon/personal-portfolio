469ad6f77af8f0d1802d56c04cc8fae8e4cc7bf7
Timothy Moon <timhgmoon@gmail.com>
2022-04-13 18:49:47 -0700

    revised about me section

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,7 @@ const About = () => {
             Software Engineer
           </p>
           <p className={aboutStyles.description}>
-            Recent graduate at General Assembly(SEI) with extensive knowledge in building websites from ground up and working with API’s. Specialize in Python and Django. Professional skills include coaching, prioritization, written and verbal communication. One of my future career goals includes stepping into a role that will help leverage my leadership skills.
+            Full-stack engineer creating responsive design for both Mobile and Web Apps with extensive knowledge in building websites from ground up and working with API's. Specialize in Python and Django. Profession skills include coaching peers on coding best practices, written and verbal communication, and task prioritization. My Future career goals include leveraging my leadership skills, building web apps utilizing python as back-end, and learning from mentors.
           </p>
     </Container>
   )
