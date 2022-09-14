8a17a8f93d27055c14fd993d6578920ed6346846
Timothy Moon <timhgmoon@gmail.com>
2022-09-13 22:04:03 -0700

    added button for project description

--- src/components/description.js
+++ src/components/description.js
@@ -1,10 +1,14 @@
 import * as React from 'react'
 import '../styles/description.sass'
-const Description = ({ header, content }) => {
+const Description = ({ header, content, github, live }) => {
   return (
     <div className="description-container">
       <h1 className="description-header">{header}</h1>
       <p className="description-content">{content}</p>
+      <div className="button-container">
+        <button className="black-button">Github</button>
+        <button className="grey-button">Live</button>
+      </div>
     </div>
   )
 }
