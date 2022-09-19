7df2cccc89dd2c21f315eb5dd122b5fe103bcabe
Timothy Moon <timhgmoon@gmail.com>
2022-09-19 15:37:04 -0700

    added functionality when clicking project

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,40 +1,38 @@
 import React, { useState } from 'react'
 import '../styles/projects.sass'
 
-const Projects = () => {
-  const [activeID, setActiveID] = useState(0)
-
-  const projects = [
-    {
-      id: 1,
-      name: "The Opportunity Project 2022",
-    },
-    {
-      id: 2,
-      name: "Daily Driver",
-    },
-    {
-      id: 3,
-      name: "Social Media Project",
-    }
-  ]
-  const handleClick = (id) => {
-    setActiveID(id)
-  }
+const Projects = ({activeID, handleID, descriptionData}) => {
+  // const projects = [
+  //   {
+  //     id: 1,
+  //     name: "The Opportunity Project 2022",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Daily Driver",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Social Media Project",
+  //   }
+  // ]
+  
   return (
     <div className="project-container">
-      <h1 className="project-header">Projects</h1>
+      <h1 className="project-header">Projects.</h1>
       <ul className="project-content">
-        {projects.map((project) => {
-          const className = activeID === project.id ? "project-item active" : "project-item"
-          return(
-            <li
-            key={project.id}
-              onClick={() => handleClick(project.id)}
-              className={className}
-            >
-              {project.name}
-            </li>
+        {descriptionData
+          .filter(project => project.id !== 0)
+          .map((project) => {
+            const className = activeID === project.id ? "project-item active" : "project-item"
+            return(
+              <li
+              key={project.id}
+                onClick={() => handleID(project.id)}
+                className={className}
+              >
+                {project.header}
+              </li>
           )
         })}
         {/* <li className="project-item">The Opportunity Project 2022</li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,30 @@
-import * as React from 'react'
+import React, { useState, useEffect } from 'react'
 import Nav from '../components/nav'
 import Projects from '../components/projects'
 import Description from '../components/description'
 import Github from '../images/github.svg'
 import Linkedin from '../images/linkedin.svg'
 import '../styles/index.sass'
+import descriptionData from '../description.json'
 
 const IndexPage = () => {
-  const header = "Hello."
-  const content = "My name is Tim, I am a full-stack developer creating modern and responsive web applications. I’m currently working on a project with General Assembly x U.S. Census Bureau to solve lead pipe issues around the U.S.  During my free time I like to listen to music, go to the gym or play games."
+  const [activeID, setActiveID] = useState(0)
+
+  const handleID = (id) => {
+    setActiveID(id)
+  }
   return (
     <main>
       <Nav></Nav>
-      <Description header={header} content={content}></Description>
-      <Projects></Projects>
+      <Description header={descriptionData[activeID].header} content={descriptionData[activeID].content}></Description>
+      <Projects activeID={activeID} handleID={handleID} descriptionData={descriptionData}></Projects>
       <footer className="footer-container">
-        <a href="/" id="github-icon"><img src={Github} width="35px" height="35px"/></a>
-        <a href="/" id="linkedin-icon"><img src={Linkedin} width="35px" height="35px"/></a>
+        <a href="/" id="github-icon">
+          <img src={Github} width="35px" height="35px" alt="github link"/>
+        </a>
+        <a href="/" id="linkedin-icon">
+          <img src={Linkedin} width="35px" height="35px" alt="linkedin link"/>
+        </a>
       </footer>
     </main>
   )
