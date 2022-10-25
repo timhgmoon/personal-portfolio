f131ff42fe45ef351bead2438ec484f2fdc79f21
Timothy Moon <timhgmoon@gmail.com>
2022-10-25 02:13:00 -0700

    changed font size/style for desktop version

--- src/components/description.js
+++ src/components/description.js
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import '../styles/description.sass'
-const Description = ({ header, content, github, live }) => {
+const Description = ({ header, stack, content, github, live }) => {
   return (
     <div className="description-container">
       <h1 className="description-header">{header}</h1>
+      <p className="description-stack">{stack}</p>
       <p className="description-content">{content}</p>
       <div className="button-container">
         <button className="black-button">Github</button>
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,16 @@ const IndexPage = () => {
     <main>
       <Nav></Nav>
       <div className="container">
-        <Description header={descriptionData[activeID].header} content={descriptionData[activeID].content}></Description>
-        <Projects activeID={activeID} handleID={handleID} descriptionData={descriptionData}></Projects>
+        <Description 
+          header={descriptionData[activeID].header} 
+          stack={descriptionData[activeID].stack} 
+          content={descriptionData[activeID].content}>
+        </Description>
+        <Projects 
+          activeID={activeID} 
+          handleID={handleID} 
+          descriptionData={descriptionData}>
+        </Projects>
       </div>
       <footer className="footer-container">
         <a href="/" id="github-icon">
